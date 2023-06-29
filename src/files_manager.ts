0f701b6c153b4787b978ff8ee4fd34b52a979951
Harminder Virk <virk.officials@gmail.com>
2023-06-29 11:50:47 +0530

    ci: remove debug logs

--- src/files_manager.ts
+++ src/files_manager.ts
@@ -48,7 +48,6 @@ export class FilesManager {
     return files.filter((file) => {
       const filename = slash(file.pathname)
       const filenameWithoutTestSuffix = filename.replace(FILE_SUFFIX_EXPRESSION, '')
-      console.log({ filename })
 
       return !!filters.find((filter) => {
         if (filename.endsWith(filter)) {
@@ -57,7 +56,6 @@ export class FilesManager {
 
         const filterSegments = filter.split('/').reverse()
         const fileSegments = filenameWithoutTestSuffix.split('/').reverse()
-        console.log({ filterSegments, fileSegments })
 
         return filterSegments.every((segment, index) => {
           return fileSegments[index] && (segment === '*' || fileSegments[index].endsWith(segment))
